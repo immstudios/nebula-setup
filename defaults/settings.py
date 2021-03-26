5da76312af35e9b2fd476733d13eb7d5a8a64c33
Martastain <martas@imm.cz>
2021-03-26 11:31:49 +0100

    some default settings moved to their respective places in the nebula code

--- defaults/settings.py
+++ defaults/settings.py
@@ -1,14 +1,7 @@
 SETTINGS = {
-    "seismic_addr"   : "224.168.1.1",
-    "seismic_port"   : 42005,
     "cache_host"     : "localhost",
     "cache_port"     : 11211,
     "plugin_storage" : 1,
     "plugin_root"    : ".nx/scripts/v5",
     "proxy_storage"  : 1,
-    "smtp_host"      : "localhost",
-    "smtp_user"      : False,
-    "smtp_pass"      : False,
-    "cg_server"      : "https://cg.immstudios.org",
-    "cg_site"        : False,
 }
